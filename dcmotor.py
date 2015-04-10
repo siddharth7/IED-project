1f65e94f655e55c3df08a1bc9016064df1562997
Siddharth Singh <siddharth7.singh@gmail.com>
2015-04-10 18:09:04 +0530

    Create dcmotor.py

---
+++ dcmotor.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+i=0
+while(i<10000) :
+    i+=1
+    #MOTOR1A=16
+    MOTOR1B=18
+    MOTOR1E=22
+
+
+    #MOTOR2A=23
+    MOTOR2B=31
+    MOTOR2E=29
+
+    #GPIO.setup(MOTOR1A, GPIO.OUT)
+    GPIO.setup(MOTOR1B, GPIO.OUT)
+    GPIO.setup(MOTOR1E, GPIO.OUT)
+
+    #GPIO.setup(MOTOR2A, GPIO.OUT)
+    GPIO.setup(MOTOR2B, GPIO.OUT)
+    GPIO.setup(MOTOR2E, GPIO.OUT)
+
+    
+    print "turning motor on"
+
+    #GPIO.output(MOTOR1A, GPIO.HIGH)
+    GPIO.output(MOTOR1B, GPIO.HIGH)
+    GPIO.output(MOTOR1E, GPIO.LOW)
+
+    #GPIO.output(MOTOR2A, GPIO.HIGH)
+    GPIO.output(MOTOR2B, GPIO.LOW)
+    GPIO.output(MOTOR2E, GPIO.HIGH)
+
+    sleep(2)
+
+    print "stopping motor"
+
+    #GPIO.output(MOTOR1E, GPIO.LOW)
+
+GPIO.cleanup()
+
+            
